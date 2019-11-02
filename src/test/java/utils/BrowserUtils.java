fa25b4804c739b5823417a4080a5ed40a8bbb68a
kadir3434 <kadiryesir@gmail.com>
2019-11-01 20:00:00 -0500

    utils 1

---
+++ src/test/java/utils/BrowserUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class BrowserUtils {
+    //it will be used to pause our test execution
+    //just provide number of seconds as a parameter
+    public static void wait(int seconds){
+        try {
+            Thread.sleep(1000 * seconds);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
