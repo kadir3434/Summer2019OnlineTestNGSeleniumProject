ffde4702cfbeac443edfeb431a5facbe17890704
kadir3434 <kadiryesir@gmail.com>
2019-11-01 20:11:01 -0500

    Vytrack 2

---
+++ src/test/java/tests/vyTrackLogin/LoginWithInvalidCredentials.java
@@ -0,0 +1,30 @@
+package tests.vyTrackLogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserFactory;
+
+public class LoginWithInvalidCredentials {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.get("https://qa2.vytrack.com/user/login");
+
+        driver.findElement(By.id("prependedInput")).sendKeys("username");
+        driver.findElement(By.id("prependedInput2")).sendKeys("password");
+
+        driver.findElement(By.id("_submit")).click();
+
+        String expectedErrorMsg = "Invalid user name or password.";
+        WebElement actualResult = driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/div[1]/div"));
+        String actualErrorMsg = actualResult.getText();
+        if(actualErrorMsg.equals(expectedErrorMsg)){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
