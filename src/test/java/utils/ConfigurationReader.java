87f39773beac276a4b0159e32443036264225dfb
kadir3434 <kadiryesir@gmail.com>
2019-11-26 14:18:46 -0600

    11/26/2019

--- src/test/java/utils/ConfigurationReader.java
+++ src/test/java/utils/ConfigurationReader.java
@@ -1,7 +1,7 @@
 package utils;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
@@ -11,15 +11,25 @@ public class ConfigurationReader {
     private static Properties configFile;
     static {
         try {
+
             //provides access to file
             //try/catch block stands for handling exceptions
             //if exception occurs, code inside a catch block will be executed
             //any class that is related to InputOutput produce checked exceptions
             //without handling checked exception, you cannot run a code
             FileInputStream fileInputStream = new FileInputStream("configuration.properties");
-        } catch (FileNotFoundException e) {
+
+            //initialize properties object
+            configFile = new Properties();
+
+            //load configuration.properties file
+            configFile.load(fileInputStream);
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    public static String getProperty(String key) {
+        return configFile.getProperty(key);
+    }
 }
